438afa06c4168d8b9b6c78031ffafe8b1cdad2d6
Hlebik <45106626+Hlebik@users.noreply.github.com>
2025-04-21 18:34:16 +0300

    Delete pages/home.tsx

--- pages/home.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex items-center justify-center min-h-screen bg-gray-100 text-xl">
-      <h1 className="text-3xl font-bold text-blue-600">Привет! Это FocusSync 🎯</h1>
-    </main>
-  )
-}
