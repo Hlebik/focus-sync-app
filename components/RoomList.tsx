cd4e4f08241e3c8fdcd4ad5ba8c70f575b18bd06
Hlebik <45106626+Hlebik@users.noreply.github.com>
2025-04-21 18:36:51 +0300

    Create RoomList.tsx

---
+++ components/RoomList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+
+const RoomList = () => {
+  const rooms = ['Room 1', 'Room 2', 'Room 3', 'Room 4', 'Room 5'];  // Пример списка комнат
+
+  return (
+    <div>
+      <h2>Available Rooms</h2>
+      <ul>
+        {rooms.map((room, index) => (
+          <li key={index}>
+            <Link href={`/room?roomId=${room}`}>
+              <a>{room}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RoomList;
